ee651b8bad81f5fae03b4b5c0043b9df484d074a
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-06-11 15:35:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("hello")
+let firstName="Wow";
+let lastName="programmer";
+console.log(firstName,lastName)
+
+// string concatenate
+// template literal
+let fullname=`I want to become ${firstName} ${lastName}`
+console.log(fullname)
+// getting string character
+console.log(firstName.replace("wow","hello7"))
+//trim
+let hobbies="     coding reading running     kvl;fl;  "
+console.log(hobbies)
+let result= hobbies.includes("helo")
+console.log(result)
+let full="aayushi mishra"
+console.log(full.slice(0,6))
+let favouritecolor= "Brown BlueBlackGray";
+let arrcolor=favouritecolor.split("")
+//string to array;
+console.log(arrcolor)
+console.log(arrcolor[2])
