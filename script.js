67a2e8558cc3f0bc289db47d9f98e1d0754f7853
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-06-11 16:04:25 +0530

    changew

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-console.log("hello")
-let firstName="Wow";
-let lastName="programmer";
-console.log(firstName,lastName)
+//loosely equality opeerator(== b/t ===)
 
-// string concatenate
-// template literal
-let fullname=`I want to become ${firstName} ${lastName}`
-console.log(fullname)
-// getting string character
-console.log(firstName.replace("wow","hello7"))
-//trim
-let hobbies="     coding reading running     kvl;fl;  "
-console.log(hobbies)
-let result= hobbies.includes("helo")
-console.log(result)
-let full="aayushi mishra"
-console.log(full.slice(0,6))
-let favouritecolor= "Brown BlueBlackGray";
-let arrcolor=favouritecolor.split("")
-//string to array;
-console.log(arrcolor)
-console.log(arrcolor[2])
+// let age= 23
+// console.log(age==="23")
+// //type conversion
+// let string ="54";
+// console.log(string, typeof string)
+// //number
+// let number= Number(string)
+// console.log(number, typeof number)
+// let str= String(number)
+// console.log(str, typeof str)
