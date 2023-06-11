8226e7bda3ddd4345c82c96362540a2a2d784d82
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-06-11 16:18:34 +0530

    changess

--- script.js
+++ script.js
@@ -10,3 +10,20 @@
 // console.log(number, typeof number)
 // let str= String(number)
 // console.log(str, typeof str)
+//arrays
+let dishes=["biriyani","paratha","bhatut",""]
+dishes[0]="roti"
+console.log(dishes)
+console.log(dishes[3])
+console.log(dishes.join(","))
+console.log(dishes.indexOf("roti"))
+let newDishes=["sweet","roshogulla","pithe"]
+
+updatedDishes=dishes.concat(newDishes)
+console.log(updatedDishes)
+console.log(updatedDishes.length)
+//push method
+console.log(updatedDishes.push("bhallay"))
+console.log(updatedDishes)
+console.log(updatedDishes.pop())
+console.log(updatedDishes)
